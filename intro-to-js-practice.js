3fcd3f457feb25257de1fc8a68fb20697115f512
Scott Sutherland <sutherlandscott21@gmail.com>
2024-07-30 11:47:31 -0600

    started tracking new file

---
+++ intro-to-js-practice.js
@@ -0,0 +1,61 @@
+// const greeting = 'Howdy!';
+// console.log(greeting);
+
+
+// let score = 0
+
+// score = 10
+
+// console.log(score)
+
+// const myName = "Scott Sutherland"
+
+// const greeting = ` say's Hi!`
+
+// console.log(myName + greeting)
+
+// const poem = `Roses are red
+// Sugar is sweet
+// His boots are too big
+// For his goshdarn feet`;
+
+// const adjective = 'gigantic';
+// const color = 'brown';
+// const animal = 'fox';
+
+// const slowWay = 'The ' + adjective + " space"
+
+// const madlib = `The ${adjective} ${color} space hamster jumped over the lazy ${animal}.`
+// console.log(madlib);
+
+// const numPrizes = 20;
+// const totalAttendees = 5;
+
+// // Show prizes per attendee
+// const prizePerAttendees = numPrizes / totalAttendees
+
+// console.log(prizePerAttendees)
+
+// let sum = 0 
+
+// sum += 10
+
+// console.log(sum)
+
+// let sleepy = true;
+
+// if (sleepy) {
+//   console.log('Yawn...');
+// } else {
+//   console.log("I'm awake!");
+// }
+
+// let username = null
+
+function addFive(num) {
+    return num + 5
+  }
+  
+  // console.log()
+  
+  console.log(addFive())
